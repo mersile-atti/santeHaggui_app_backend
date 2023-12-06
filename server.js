58aadfa52f698f5f6051f0f6ce92a366a6510ab4
Oumar Mersile Atti Mahamat <74494155+mersile-atti@users.noreply.github.com>
2023-12-06 02:50:25 +0100

    changed start script

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ dotenv.config();
 import cookieParser from 'cookie-parser';
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js';
-const port = process.env.PORT || 5003
+const port = process.env.PORT || 5000
 import userRoutes from './routes/userRoutes.js'
 
 connectDB();
