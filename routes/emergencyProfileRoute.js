64f4670daa63c13627a1fc0375c04724b57a5055
Oumar Mersile Atti Mahamat <74494155+mersile-atti@users.noreply.github.com>
2023-12-07 04:39:45 +0100

    Emergency Profile Fixed

--- routes/emergencyProfileRoute.js
+++ routes/emergencyProfileRoute.js
@@ -1,10 +1,9 @@
 // routes/emergencyProfileRoute.js
 import express from 'express';
 import { createOrUpdateEmergencyProfile } from '../controllers/emergencyProfileController.js';
-import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(protect, createOrUpdateEmergencyProfile);
+router.route('/').post(createOrUpdateEmergencyProfile);
 
 export default router;
